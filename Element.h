9791f5a2d4b170a399194870b5e5a2c19c243041
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-04-28 16:40:07 -0700

    Update Element.h

--- Element.h
+++ Element.h
@@ -4,32 +4,14 @@
 #include <string>
 using std::string;
 
+
 class Element
 {
 public:
-    Element(string name,unsigned short id,Element* parent){
-        _name = name;
-        _id = id;
-        _parent = parent;
-        _hasChildren = false;
-        _childCount = 0;
-        _next = nullptr;
-        _previous = nullptr;
-        _childHead = nullptr;
-        
-    };
-    ~Element(){};
+    Element(string name,unsigned short id,Element* parent);
+    ~Element();
     
-    Element(const Element& other){
-        _name = other._name;
-        _id = other._id;
-        _parent = other._parent;
-        _hasChildren = other._hasChildren;
-        _parent = other._parent;
-        _next = other._next;
-        _childHead = other._childHead;
-        _previous = other._previous;
-    };
+    Element(const Element& other);
     
     bool hasChildren(){return _hasChildren;}
     int _childCount;
