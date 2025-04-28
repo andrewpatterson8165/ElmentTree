a5e06e274a4c0c3242dcda8b9e597f3e460a7ec0
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-04-27 18:50:28 -0700

    Update Element.cpp

--- Element.cpp
+++ Element.cpp
@@ -1,8 +1,27 @@
 #include "Element.h"
-
+///TODO - Investigate to see if this should be migrated to a differnt class
 void Element::addChild(Element* child)
 {
-    
+    if(child != nullptr)
+    {
+        if(_childCount == 0)
+        {
+            _childHead = child;
+            _currentChild = _childHead;
+            _childCount++;
+        }
+        else //If children actually exist
+        {
+            _currentChild->_next = child;
+            _currentChild = _currentChild->_next;
+            _childCount++;
+        }
+    }
+    //Element* _parent;
+    //Element* _next;
+    //Element* _previous;
+    //Element* _childHead;
+    //Element* _currentChild;
 }
 
 
