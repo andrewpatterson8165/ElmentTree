864519291efd1c1e6bfa7dab4291f294970f6e94
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-04-27 18:40:31 -0700

    Update Element.h

--- Element.h
+++ Element.h
@@ -10,22 +10,26 @@ public:
         _name = name;
         _id = id;
         _parent = parent;
+        _hasChildren = false;
+        _childCount = 0;
     };
     ~Element(){};
     Element(const Element& other){};
-    
+    bool hasChildren(){return _hasChildren;}
+    void addChild(Element* child);
+    int _childCount;
     string _name;
     unsigned short _id;
     unsigned int _handle;
-    
+    bool _hasChildren;    
     
     Element* _parent;
-    
     Element* _next;
-    
     Element* _previous;
-    
     Element* _childHead;
+    Element* _currentChild;
+
+
     
     
     
