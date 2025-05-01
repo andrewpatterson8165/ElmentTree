8085312d378ee8adc3da8a16453d11a517bdceeb
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-01 04:14:32 -0700

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -1,90 +1,96 @@
 #include <iostream>
 
-// Node structure for the binary tree
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    TreeNode* left;
+    TreeNode* right;
 
-    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+    TreeNode(T data) : data(data), left(nullptr), right(nullptr) {}
+    ~TreeNode() {
+        delete left;
+        delete right;
+    }
 };
 
-// Function to insert a new node into the binary tree
-Node* insert(Node* root, int data) {
-    if (root == nullptr) {
-        return new Node(data);
-    }
+template <typename T>
+class BinaryTree {
+public:
+    TreeNode<T>* root;
 
-    if (data < root->data) {
-        root->left = insert(root->left, data);
-    } else {
-        root->right = insert(root->right, data);
-    }
-    return root;
-}
-
-// Function for inorder traversal (Left, Root, Right)
-void inorderTraversal(Node* root) {
-    if (root != nullptr) {
-        inorderTraversal(root->left);
-        std::cout << root->data << " ";
-        inorderTraversal(root->right);
-    }
-}
-
-// Function for preorder traversal (Root, Left, Right)
-void preorderTraversal(Node* root) {
-    if (root != nullptr) {
-        std::cout << root->data << " ";
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
-    }
-}
-
-// Function for postorder traversal (Left, Right, Root)
-void postorderTraversal(Node* root) {
-    if (root != nullptr) {
-        postorderTraversal(root->left);
-        postorderTraversal(root->right);
-        std::cout << root->data << " ";
-    }
-}
-
-// Function to free the memory allocated for the tree
-void freeTree(Node* root) {
-    if (root != nullptr) {
-        freeTree(root->left);
-        freeTree(root->right);
+    BinaryTree() : root(nullptr) {}
+    ~BinaryTree() {
         delete root;
     }
-}
+
+    void insert(T data) {
+        root = insertRecursive(root, data);
+    }
+
+    TreeNode<T>* search(T data) {
+        return searchRecursive(root, data);
+    }
+
+    void printInOrder() {
+        printInOrderRecursive(root);
+        std::cout << std::endl;
+    }
+
+private:
+    TreeNode<T>* insertRecursive(TreeNode<T>* node, T data) {
+        if (node == nullptr) {
+            return new TreeNode<T>(data);
+        }
+
+        if (data < node->data) {
+            node->left = insertRecursive(node->left, data);
+        } else {
+            node->right = insertRecursive(node->right, data);
+        }
+        return node;
+    }
+
+    TreeNode<T>* searchRecursive(TreeNode<T>* node, T data) {
+        if (node == nullptr || node->data == data) {
+            return node;
+        }
+
+        if (data < node->data) {
+            return searchRecursive(node->left, data);
+        } else {
+            return searchRecursive(node->right, data);
+        }
+    }
+    
+    void printInOrderRecursive(TreeNode<T>* node) {
+        if (node != nullptr) {
+            printInOrderRecursive(node->left);
+            std::cout << node->data << " ";
+            printInOrderRecursive(node->right);
+        }
+    }
+};
 
 int main() {
-    Node* root = nullptr;
-
-    // Inserting nodes
-    root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 20);
-    insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
+    BinaryTree<int> tree;
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(70);
+    tree.insert(60);
+    tree.insert(80);
 
     std::cout << "Inorder traversal: ";
-    inorderTraversal(root);
-    std::cout << std::endl;
+    tree.printInOrder();
 
-    std::cout << "Preorder traversal: ";
-    preorderTraversal(root);
-    std::cout << std::endl;
-
-    std::cout << "Postorder traversal: ";
-    postorderTraversal(root);
-    std::cout << std::endl;
-
-    // Free allocated memory
-    freeTree(root);
+    TreeNode<int>* node = tree.search(40);
+    if (node != nullptr) {
+        std::cout << "Found node: " << node->data << std::endl;
+    } else {
+        std::cout << "Node not found" << std::endl;
+    }
 
     return 0;
 }
