ede97a16353e99e9f912c86103574f77a99500b3
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-05 16:51:12 -0700

    Update mainTest.cpp

--- mainTest.cpp
+++ mainTest.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
-#include "ElementTree.h"
+#include "Element.h"
+
 using std::endl;
 using std::cout;
+
+
 int main()
 {
-   
-    Element mainWindow("MainWindow",001,nullptr);
-    ElementList mwChildList;
+    Element mainWindow("mainWindow", 01,nullptr);
+    Element fileDialog("fileDialog", 100,&mainWindow);
+    
+    Element fileSaveAsButton("fileSaveAsButton",204,&fileDialog);
+    Element fileCancelButton("fileCancelButton",205,&fileDialog);
+    
+    Element editDialog("editDialog", 101,&mainWindow);
+    Element searchButton("searchButton",201,&editDialog);
+    Element findButton("findButton",202,&editDialog);
+    Element replaceButton("replaceButton",203,&editDialog);
 
+    Element fontDialog("fontDialog", 102,&mainWindow);
+    Element aboutDialog("aboutDialog,",103,&mainWindow);
     
     
-    ElementTree et(&mainWindow);
-    Element fileDialog("FileDialog",10, &mainWindow);
+    cout << "Unit Test" << endl;
     
-    mainWindow.setChildListHead(&fileDialog);
-    mwChildList.setHead(&fileDialog);
-    mainWindow.setListPtr(&mwChildList);
-    Element searchDialog("SearchDialog",11,&mainWindow);
-    mwChildList.addElement(&searchDialog);
-    
-    if(mwChildList.getHead() == &fileDialog)
+    mainWindow.addChildElement(&fileDialog);
+    mainWindow.addChildElement(&editDialog);
+    mainWindow.addChildElement(&fontDialog);
+    mainWindow.addChildElement(&aboutDialog);
+    cout << "Number of Elements = " << mainWindow.getChildList().size() << endl;
+    Element* ref = mainWindow.findChildElement("fileDialog");
+    if(ref != nullptr)
     {
-        cout << "Child List Head test -- PASS" << endl;
+        ref->addChildElement(&fileSaveAsButton);
+        ref->addChildElement(&fileCancelButton);
+        cout << "Number Of elements = " << ref->getChildList().size() << endl;
+        
     }
-    else
-    {
-        cout << "Child List Head Fail"  << endl;
-    }
+    
+    
     
 
-    
     return 0;
 }
