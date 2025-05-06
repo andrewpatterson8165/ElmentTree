3dfec6c4391aa0c9017e8ab4662b108cf858955c
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-05 18:10:20 -0700

    Update WinDef.h

--- WinDef.h
+++ WinDef.h
@@ -4,4 +4,35 @@
 typedef unsigned short UINT16;
 typedef unsigned int HANDLE;
 
+
+
+bool findInTree(Element* itr, string name)
+{
+    bool flag = false;
+    if(itr == nullptr)
+    {
+        return false;
+    }
+    if(itr->getName() == name)
+    {
+        return true;
+    }
+    else
+    {
+        if(itr->findChildElement(name))
+        {
+            return true;
+        }
+        else
+        {
+            Element* ref = itr->getChildList().front();
+            flag = findInTree(ref,name);
+            if(flag)
+            {
+                return true;
+            }
+        }
+    }
+    return flag;
+}
 #endif
