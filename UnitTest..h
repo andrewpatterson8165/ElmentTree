56d60f6a72e133088c0db4065eb8e846f77f9e7f
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-10 05:48:04 -0700

    Update UnitTest..h

--- UnitTest..h
+++ UnitTest..h
@@ -1,15 +1,36 @@
-#ifndef UNITTEST_H
-#define UNITTEST_H
-
+#ifndef UNITTEST_H_
+#define UNITTEST_H_
 
 #include "Element.h"
 
 
-
-void testMain();
-
-bool findInTree(Element* itr, string name);
-
-
+class UnitTest
+{
+public:
+    UnitTest();
+    ~UnitTest();
+    UnitTest(const UnitTest& other);
+    
+    void initVars();
+    void test1();
+    void test2();
+    void void3();
+private:
+    Element* _mainWindow;
+    Element* _fileDialog;
+    Element* _fileDialogSaveButton;
+    Element* _fileDialogCancelButton;
+    Element* _findReplaceDialog;
+    Element* _findReplaceDialogFindButton;
+    Element* _findReplaceDialogReplaceButton;
+    Element* _fontDialog;
+    Element* _fontDialogOkButton;
+    Element* _fontDialogColorButton;
+    Element* _fontDialogSizeButton;
+    Element* _aboutDialog;
+    Element* _aboutScrollButton
+    Element* _aboutOkButton;
+    
+};
 
 #endif
