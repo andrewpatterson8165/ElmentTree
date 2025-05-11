757aad5d9c4ab47e0ba7f93e29fb5964415784ba
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-11 06:10:45 -0700

    Update ElementTree.cpp

--- ElementTree.cpp
+++ ElementTree.cpp
@@ -46,3 +46,46 @@ bool ElementTree::findInTree(Element* itr, string name)
     }
     return flag;
 }
+
+
+bool ElementTree::findInTree(Element* itr, UINT16 resourceId)
+{
+    bool flag = false;
+    if(itr == nullptr)
+    {
+        return false;
+    }
+    if(itr->getResourceId() == resourceId)
+    {
+        return true;
+    }
+    else
+    {
+        Element* ref = itr->findChildElement(resourceId);
+        if(ref != nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            for(auto it : itr->getChildList())
+            {
+                if(it->hasChildren())
+                {
+                    cout << "child Name ="  << it->getName() << endl;
+                    cout << "Iterator Has Children Flag = true"  << endl;
+                   
+                    flag = findInTree(it,resourceId);
+                    if(flag)
+                    {
+                        cout << "Match Found"  << endl;
+                        return true;
+                    }
+                }
+               
+            }
+        }
+    
+    }
+    return flag;
+}
