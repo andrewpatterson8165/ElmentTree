a001fe88183b6539cce7256bb0ab17cdfc67de21
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-12 16:18:22 -0700

    Update WinDef.h

--- WinDef.h
+++ WinDef.h
@@ -4,5 +4,10 @@
 typedef unsigned short UINT16;
 typedef unsigned int HANDLE;
 
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned long DWORD;
+typedef DWORD COLORREF;
+
 
 #endif
