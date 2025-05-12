914c741feace06a59017506c18f275a9fc917523
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-12 15:37:39 -0700

    Update Color.h

--- Color.h
+++ Color.h
@@ -1,1 +1,36 @@
+#ifndef COLOR_H_
+#define COLOR_H_
+
+#include "WinDef.h"
+
+class Color
+{
+public:
+    Color();
+    Color(BYTE red,BTYE green,BYTE blue);
+    Color(COLORREF color);
+    Color(const Color& other);
+    ~Color();
+
+    
+
+    BYTE getRed();
+    void setRed(BYTE red);
+
+    BYTE getGreen();
+    void setGreen(BYTE green);
+
+    BYTE getYellow();
+    void setYellow(BYTE yellow);    
+
+private:
+    BYTE _red;
+    BYTE _green;
+    BYTE _blue;
+    COLORREF _value;
+
+
+}
+
+#endif
 
