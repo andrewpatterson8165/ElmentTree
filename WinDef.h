b3cfdd85a921285f3e34c0c1b311411e86c4413d
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-15 14:55:58 -0700

    Update WinDef.h

--- WinDef.h
+++ WinDef.h
@@ -1,13 +1,15 @@
 #ifndef WINDEF_H_
 #define WINDEF_H_
+#include <cstdint>
 
 typedef unsigned short UINT16;
 typedef unsigned int HANDLE;
 
-typedef unsigned char BYTE;
+typedef uint8_t BYTE;
 typedef unsigned short WORD;
 typedef unsigned long DWORD;
+typedef int64_t INT64; //Required for Windows 11
+typedef uint64_t UINT64; //Required for Windows 11
 typedef DWORD COLORREF;
-
 
 #endif
