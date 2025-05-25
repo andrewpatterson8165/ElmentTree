1c355111fe7bd4b86809232a79f03b0e8577a751
andrewpatterson8165 <andrewpatterson8165@gmail.com>
2025-05-25 11:34:44 -0700

    Update WinDef.h

--- WinDef.h
+++ WinDef.h
@@ -2,11 +2,9 @@
 #define WINDEF_H_
 #include <cstdint>
 
-
 #define CONST const
 
 typedef unsigned short UINT16;
-
 
 typedef uint8_t BYTE;
 typedef unsigned short WORD;
@@ -16,18 +14,23 @@ typedef uint64_t UINT64; //Required for Windows 11
 typedef DWORD COLORREF;
 typedef void *PVOID;
 typedef PVOID HANDLE;
+typedef HANDLE HICON;
+typedef HANDLE HBRUSH;
+typedef HANDLE HCURSOR;
+
+
 typedef char CHAR;
 typedef CHAR* PSTR;
 
-
 typedef wchar_t WCHAR;
 typedef CONST WCHAR *LPCWSTR;
+typedef const char* LPCSTR;
+
 typedef LPCWSTR LPCTSTR;
 
 typedef wchar_t WCHAR;
 typedef long* LONG_PTR;
 typedef LONG_PTR LPARAM;
-
 
 typedef HANDLE HINSTANCE;
 typedef HANDLE HWND;
@@ -36,6 +39,5 @@ typedef UINT* WPARAM;
 typedef long LONG;
 typedef long* LONG_PTR;
 typedef LONG_PTR LRESULT;
-
 
 #endif
